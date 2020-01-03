0f80e061810453eaff0a2ca712a2b92d1fce3090
cyk90290 <57485322+cyk90290@users.noreply.github.com>
2020-01-03 14:58:37 +0800

    Update MyClass.java

--- lib/src/main/java/com/example/lib/MyClass.java
+++ lib/src/main/java/com/example/lib/MyClass.java
@@ -1,35 +1,36 @@
 package com.example.lib;
 import java.util.Scanner;
 public class MyClass {
-
-    public static void main(String[] args) {
-        int Year=0,Month=0,Day=0,TotalDay=0;
-        int MonthDay[]={31,28,31,30,31,30,31,31,30,31,30,31};
-        boolean LeapYear=false;
+public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("請輸入西元年 月 日");
-        Year = scanner.nextInt();
-        Month = scanner.nextInt();
-        Day = scanner.nextInt();
-        if(Year%4!=0)
-            LeapYear=false;
-        else if(Year%100!=0)
-            LeapYear=true;
-        else if(Year%400!=0)
-            LeapYear=false;
-        else
-            LeapYear=true;
-        if(LeapYear==true)
-            MonthDay[1]=29;
-        if(Month>12)
-            System.out.println("請輸入正確月份!");
-        else if(MonthDay[Month-1]<Day)
-            System.out.printf("輸入錯誤!!%d年的%d月只有%d天\n",Year,Month,MonthDay[Month-1]);
-        else {
-            for(int i=Month-2;i>=0;--i){
-                TotalDay+=MonthDay[i];
+        System.out.println("請輸入一字串(<100)");
+        String inputstring = scanner.next();
+        String LongestStandard = "";
+        int LongestLength = 0;
+        System.out.println(inputstring);
+        System.out.println(inputstring.substring(1, 3));
+        int InputstringLength = inputstring.length();
+
+        for (int i = 0; i < InputstringLength; ) {
+            for (int j = 2; j < InputstringLength + 1; ) {                      //(若由後往前找得值等同起始值，可知沒有一個相同)
+                if (inputstring.lastIndexOf(inputstring.substring(i, j)) == i) {//後面沒有相同的
+                    i++;                                                //往下找
+                    j = i + 2;                                          //倆倆先找
+                    if (j >= InputstringLength + 1) {
+                        i = InputstringLength;                          //字串收尋結束直接強迫結束迴圈
+                    }
+                } else {
+                    int LN = inputstring.substring(i, j).length();
+                    if (LN > LongestLength) {                           //如果有相同長度的字串大於"最長字串長度"
+                        LongestStandard = inputstring.substring(i, j);  //將字串覆蓋於標準字串
+                        LongestLength = LN;                             //將"最長字串長度"填入該長度
+                    } else if (LN == LongestLength) {                   //若有長度相同
+                        LongestStandard = LongestStandard + "," +       //進行字串增長
+                                inputstring.substring(i, j);
+                    }
+                    j++;
+                }
             }
-            TotalDay+=Day;
-            System.out.printf("這天是今年第%d天\n",TotalDay);
         }
+        System.out.println("最常重複字串:" + LongestStandard);
     }}
